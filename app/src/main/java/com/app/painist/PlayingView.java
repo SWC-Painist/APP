6c5359cb01334c38fd8ae00127dbc92ff2155989
Criheacy <1052999141@qq.com>
2021-02-28 19:31:34 +0800

    Refactor Canvas Structure; Implement Basic Scrolling Component

--- app/src/main/java/com/app/painist/PlayingView.java
+++ app/src/main/java/com/app/painist/PlayingView.java
@@ -11,24 +11,28 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Build;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
 import org.jetbrains.annotations.NotNull;
 
+import static java.lang.Float.NaN;
+
 class PlayingView extends View {
 
-    private PlayingActivity attachedActivity;
+    protected PlayingActivity attachedActivity;
+    protected MainScoreRenderer mainScoreRenderer;
 
-    private Paint mPaint;
-    private Bitmap mainScore;
+    protected Paint mPaint;
 
-    private float mWidth;
-    private float mHeight;
+    protected float mWidth;
+    protected float mHeight;
 
-    private float rotateCenterX;
-    private float rotateCenterY;
+    // 上一次触摸位置（用于跟踪偏移量）
+    private float lastTouchPos;
 
     public PlayingView(@NotNull Activity activity) {
         super(activity.getBaseContext());
@@ -37,13 +41,37 @@ class PlayingView extends View {
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(10f);
 
+        mainScoreRenderer = new MainScoreRenderer();
+
         mWidth = 0f;
         mHeight = 0f;
 
-        attachedActivity = (PlayingActivity) activity;
+        lastTouchPos = NaN;
 
+        attachedActivity = (PlayingActivity) activity;
         setLayerType(LAYER_TYPE_SOFTWARE, mPaint);
-        mainScore = BitmapFactory.decodeResource(getResources(), R.mipmap.oneline_score);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        float x = event.getX();
+        float y = event.getY();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mainScoreRenderer.setHoldingState(true);
+                break;
+            case MotionEvent.ACTION_UP:
+                mainScoreRenderer.setHoldingState(false);
+                lastTouchPos = NaN;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (!Float.isNaN(lastTouchPos))
+                    mainScoreRenderer.setHoldingOffset(y - lastTouchPos);
+                lastTouchPos = y;
+                break;
+        }
+        return true;
     }
 
     @Override @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -53,23 +81,20 @@ class PlayingView extends View {
         mWidth = getWidth();
         mHeight = getHeight();
 
-        drawMainScore(canvas);
+        mainScoreRenderer.renderMainScore(canvas, mWidth / 2, mHeight / 2);
 
-        canvas.drawLine(0, 0, mWidth, mHeight, mPaint);
-        canvas.drawLine(0, mHeight, mWidth, 0, mPaint);
-        float screenRotation = attachedActivity.getScreenRotation();
-
+//        float screenRotation = attachedActivity.getScreenRotation();
 //        if (screenRotation == 0f || screenRotation == 180f) {
 //            Log.d("ScreenRotation", "screen rotation false");
 //            drawRotationSuggestion(canvas);
 //        }
 
         invalidate();
-        Log.d("tmp", String.valueOf(System.currentTimeMillis()));
+//        Log.d("tmp", String.valueOf(System.currentTimeMillis()));
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void drawRotationSuggestion(Canvas canvas) {
+    private void drawRotationSuggestion(@NotNull Canvas canvas) {
         Paint tPaint = new Paint();
         tPaint.setColor(Color.GRAY);
         tPaint.setAlpha(150);
@@ -93,14 +118,128 @@ class PlayingView extends View {
         canvas.drawBitmap(tRotateIcon, src, dist, tPaint);
     }
 
-    private void drawMainScore(Canvas canvas) {
-        Matrix matrix = new Matrix();
-        matrix.postTranslate(-mainScore.getWidth() / 2f, -mainScore.getHeight() / 2f);
-        matrix.postRotate(90);
-        matrix.postScale(1.5f, 1.5f);
-        matrix.postTranslate(this.getWidth() / 2f,this.getHeight() / 2f);
+    class MainScoreRenderer {
+        private Bitmap mainScore;
 
-        canvas.drawBitmap(mainScore, matrix, null);
-        matrix.reset();
+        // 交互属性
+        private boolean mHolding;   // 是否正在拖拽（按住）图片位置
+        private int holdingOffset;  // 上一次拖拽的位移
+
+        // 渲染最大最小偏移量，取决于图片尺寸
+        private int minOffset;
+        private int maxOffset;
+
+        // 运动属性：用于决定渲染偏移量mOffset
+        private long lastUpdateTime;
+
+        private int offset;
+        private double velocity;
+        private double accel;
+
+        private double constraint;  // 在松手时的滑动摩擦力
+        private double rebound;     // 在边界处的回弹力
+
+        public MainScoreRenderer() {
+            // 暂时使用oneline_score图片代替
+            mainScore = BitmapFactory.decodeResource(getResources(), R.mipmap.oneline_score);
+
+            minOffset = 0;
+            maxOffset = mainScore.getWidth();
+            Log.d("MaxOffset", String.valueOf(maxOffset));
+
+            lastUpdateTime = System.currentTimeMillis();
+            offset = 0;
+            velocity = 0;
+            accel = 0;
+
+            constraint = 9;
+            rebound = 1;
+        }
+
+        public void setHoldingState(boolean isHolding) {
+            mHolding = isHolding;
+        }
+
+        public void setHoldingOffset(float holdingOffset) {
+            if (mHolding)
+                this.holdingOffset = (int) holdingOffset;
+        }
+
+        private void updateAccel(float deltaTime) {
+            accel = 0;
+
+            // 计算滑动摩擦加速度
+            if (velocity != 0 && !mHolding) {
+                accel -= velocity * (1 - 1 / (constraint + 1));
+                accel -= Math.abs(velocity) * constraint * 0.05 / velocity;
+            }
+
+            // 限制最大加速度
+//            if (accel > 500 * constraint) {
+//                accel = 500 * constraint;
+//            }
+//            if (accel < -500 * constraint) {
+//                accel = -500 * constraint;
+//            }
+        }
+
+        private void updateVelocity(float deltaTime) {
+            updateAccel(deltaTime);
+            if (Math.abs(accel) > Math.abs(velocity)) {
+                velocity = 0;
+            }
+            else {
+                velocity += accel * deltaTime;
+            }
+        }
+
+        private void updateOffset(float deltaTime) {
+            if (mHolding) {
+                velocity = 0;
+                updateVelocity(deltaTime);
+                velocity += holdingOffset;
+                holdingOffset = 0;
+                offset += velocity;
+            }
+            else {
+                updateVelocity(deltaTime);
+                offset += velocity;
+            }
+
+            if (-offset < minOffset) {
+                offset = -minOffset;
+                velocity = 0;
+                accel = 0;
+            }
+            if (-offset > maxOffset) {
+                offset = -maxOffset;
+                velocity = 0;
+                accel = 0;
+            }
+        }
+
+        public void renderMainScore(Canvas canvas, float dx, float dy) {
+            long nowTime = System.currentTimeMillis();
+            updateOffset((nowTime - lastUpdateTime) / 1000f);
+            lastUpdateTime = nowTime;
+
+            Matrix matrix = new Matrix();
+            matrix.postTranslate(-mainScore.getWidth() / 2f, -mainScore.getHeight() / 2f);
+            matrix.postRotate(90);
+            matrix.postScale(1.5f, 1.5f);
+            matrix.postTranslate(dx, dy + maxOffset / 2 + offset);
+
+            canvas.drawBitmap(mainScore, matrix, null);
+
+            mPaint.setColor(Color.BLACK);
+            mPaint.setTextSize(50f);
+            mPaint.setStyle(Paint.Style.FILL);
+
+            canvas.drawText("offset: "+String.valueOf(offset), mWidth - 500f, 100f, mPaint);
+            canvas.drawText("velocity: "+String.valueOf(velocity), mWidth - 500f, 200f, mPaint);
+            canvas.drawText("accel: "+String.valueOf(accel), mWidth - 500f, 300f, mPaint);
+
+            matrix.reset();
+        }
     }
 }
