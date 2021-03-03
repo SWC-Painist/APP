21f678ace005455d4647bc4b1523c9d91dd250bb
Criheacy <1052999141@qq.com>
2021-03-03 14:14:37 +0800

    Refactor Other Classes

---
+++ app/src/main/java/com/app/painist/Score.java
@@ -0,0 +1,10 @@
+package com.app.painist;
+
+/* class Score
+ * 记录乐谱当前状态，以共ScoreRenderer渲染
+ **************
+ * by Criheacy
+ * last-edit: 2021/3/3 14:08
+ */
+public class Score {
+}
---
+++ app/src/main/java/com/app/painist/ScoreStateUpdater.java
@@ -0,0 +1,12 @@
+package com.app.painist;
+
+/* class ScoreStateUpdater
+ * 用于实现onNoteHeard等回调函数，从而更改乐谱当前状态
+ * 回调函数向外连接到MusicListener，在演奏到当前音符时调用
+ **************
+ * by Criheacy
+ * last-edit: 2021/3/3 14:11
+ */
+public class ScoreStateUpdater {
+
+}
