6e791555991a15226c72b3f383c3645385fb6580
user <1027906524@qq.com>
2021-03-07 15:22:46 +0800

    add Setting Fragment and Activity

--- app/src/main/java/com/app/painist/ui/setting/SettingFragment.java
+++ app/src/main/java/com/app/painist/ui/setting/SettingFragment.java
@@ -2,6 +2,7 @@ package com.app.painist.ui.setting;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,13 +12,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.app.painist.MainActivity;
 import com.app.painist.R;
+import com.app.painist.ui.setting.activity.SettingPersonInformationActivity;
 
 public class SettingFragment extends Fragment {
 
     private SettingViewModel mViewModel;
-
+    private TextView SettingPersonInformation;
     public static SettingFragment newInstance() {
         return new SettingFragment();
     }
@@ -33,6 +37,14 @@ public class SettingFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(SettingViewModel.class);
         // TODO: Use the ViewModel
+        SettingPersonInformation = getActivity().findViewById(R.id.information);
+        SettingPersonInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),SettingPersonInformationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/app/painist/ui/setting/activity/SettingPersonInformationActivity.java
@@ -0,0 +1,16 @@
+package com.app.painist.ui.setting.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.painist.R;
+
+public class SettingPersonInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_person_information);
+    }
+}
