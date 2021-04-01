3c8c2eb9a4a911194ecc9f2a689f6da68db8865e
Criheacy <1052999141@qq.com>
2021-04-01 15:26:15 +0800

    Update statistic layout

--- app/src/main/java/com/app/painist/StatisticActivity.java
+++ app/src/main/java/com/app/painist/StatisticActivity.java
@@ -1,30 +1,40 @@
 package com.app.painist;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Interpolator;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class StatisticActivity extends AppCompatActivity {
 
-    View practiceTime;
-    View practiceCount;
-    View practiceScore;
-    View practiceAdvancement;
-    View lastMonth;
+    View[] fragmentList = new View[5];
+    int[] textGroupNumber = new int[] {5, 0, 0, 0, 0};
+
+    ValueAnimator pageTimer;
 
     FrameLayout bottomView;
     FrameLayout topView;
@@ -44,53 +54,51 @@ public class StatisticActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
 
-        LinearLayout container = (LinearLayout) findViewById(R.id.scoreitem_container);
-
         LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        practiceTime = layoutInflater.inflate(R.layout.sublayout_practice_time, null);
-        practiceCount = layoutInflater.inflate(R.layout.sublayout_practice_count, null);
-        practiceScore = layoutInflater.inflate(R.layout.sublayout_practice_score, null);
-        practiceAdvancement = layoutInflater.inflate(R.layout.sublayout_practice_advancement, null);
-        lastMonth = layoutInflater.inflate(R.layout.sublayout_last_month, null);
+        fragmentList[0] = layoutInflater.inflate(R.layout.sublayout_practice_time, null);
+        fragmentList[1] = layoutInflater.inflate(R.layout.sublayout_practice_count, null);
+        fragmentList[2] = layoutInflater.inflate(R.layout.sublayout_practice_score, null);
+        fragmentList[3] = layoutInflater.inflate(R.layout.sublayout_practice_advancement, null);
+        fragmentList[4] = layoutInflater.inflate(R.layout.sublayout_last_month, null);
 
         bottomView = findViewById(R.id.statistic_bottom_view);
         topView = findViewById(R.id.statistic_top_view);
+
+        View container = findViewById(R.id.statistic_container);
+
+        container.post(new Runnable() {
+            @Override
+            public void run() {
+                int height = container.getHeight();
+
+                Log.d("HEIGHT", String.valueOf(height));
+
+                pageTimer = new ValueAnimator();
+                pageTimer.setIntValues(0, height);
+                pageTimer.setDuration(750);
+                pageTimer.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        Log.d("Update", String.valueOf((int) animation.getAnimatedValue()));
+                        topView.setTranslationY(-(int) animation.getAnimatedValue());
+                    }
+                });
+                pageTimer.setInterpolator(new Interpolator() {
+                    @Override
+                    public float getInterpolation(float input) {
+                        return (float) Math.pow(input, 3);
+                    }
+                });
+            }
+        });
+
+        ((Button) findViewById(R.id.statistic_next_page_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pageTimer.start();
+            }
+        });
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                MouseState.isPressed = true;
-                MouseState.pressedY = event.getY();
-                Log.d("Motion", "DOWN");
-                break;
-            case MotionEvent.ACTION_UP:
-                MouseState.isPressed = false;
-                MouseState.isMoving = false;
-                Log.d("Motion", "UP");
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if (MouseState.isPressed) {
-                    Log.d("Motion", String.valueOf(MouseState.movingVelocityY));
-                    if (Math.abs(event.getY() - MouseState.pressedY) >= MouseState.startMovingDist) {
-                        MouseState.movingDown = event.getY() > MouseState.pressedY;
-                        MouseState.isMoving = true;
-                    }
-                    if (MouseState.isMoving) {
-                        if (MouseState.movingY != 0)
-                            MouseState.movingVelocityY = event.getY() - MouseState.movingY;
-                        MouseState.movingY = event.getY();
-
-                        topView.setTranslationY(MouseState.movingY - MouseState.pressedY);
-                    }
-                }
-                break;
-            default:
-                break;
-        }
-        // Log.d("TOUCH", event.getX() + "," + event.getY());
-        return false;
-    }
 }
